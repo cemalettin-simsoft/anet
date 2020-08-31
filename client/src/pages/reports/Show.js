299432a861a6bff15a35b380cbc241aedc209d97
Cemalettin Taş <cemalettin.tas@simsoft.com.tr>
2020-08-31 11:36:49 +0300

    NCI-Agency/anet#50: Change tests and class names
    Be consistent with the new naming "Compact"

--- client/src/pages/reports/Show.js
+++ client/src/pages/reports/Show.js
@@ -377,7 +377,7 @@ const ReportShow = ({ setSearchQuery, pageDispatchers }) => {
               <Button onClick={toggleEmailModal}>Email report</Button>
             )}
             <Button
-              value="printView"
+              value="compactView"
               type="button"
               bsStyle="primary"
               onClick={onCompactClick}
