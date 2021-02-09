07558afab99d4aedb13e271991a2743eba95c83a
Gertjan van Oosten <gertjan@west.nl>
2021-02-09 11:37:10 +0100

    Adapt to changed API for GraphQL SPQR update to 0.11.x

--- src/main/java/mil/dds/anet/graphql/DateTimeMapper.java
+++ src/main/java/mil/dds/anet/graphql/DateTimeMapper.java
@@ -2,8 +2,9 @@ package mil.dds.anet.graphql;
 
 import graphql.schema.GraphQLScalarType;
 import io.leangen.graphql.generator.BuildContext;
-import io.leangen.graphql.generator.OperationMapper;
+import io.leangen.graphql.generator.mapping.TypeMappingEnvironment;
 import io.leangen.graphql.generator.mapping.common.CachingMapper;
+import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.AnnotatedType;
 import java.time.Instant;
 
@@ -13,18 +14,18 @@ public class DateTimeMapper extends CachingMapper<GraphQLScalarType, GraphQLScal
 
   @Override
   public GraphQLScalarType toGraphQLType(String typeName, AnnotatedType javaType,
-      OperationMapper operationMapper, BuildContext buildContext) {
+      TypeMappingEnvironment env) {
     return GraphQLInstant;
   }
 
   @Override
   public GraphQLScalarType toGraphQLInputType(String typeName, AnnotatedType javaType,
-      OperationMapper operationMapper, BuildContext buildContext) {
-    return toGraphQLType(typeName, javaType, operationMapper, buildContext);
+      TypeMappingEnvironment env) {
+    return toGraphQLType(typeName, javaType, env);
   }
 
   @Override
-  public boolean supports(AnnotatedType type) {
+  public boolean supports(AnnotatedElement element, AnnotatedType type) {
     return type.getType() == Instant.class;
   }
 
